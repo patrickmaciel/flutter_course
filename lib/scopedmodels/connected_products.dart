9cfbab2d8936665f9ef815ea7002b5bf8f2179ce
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-04-01 08:45:11 -0300

    Obtendo o ID do POST response

--- lib/scopedmodels/connected_products.dart
+++ lib/scopedmodels/connected_products.dart
@@ -18,18 +18,22 @@ mixin ConnectedProductsModel on Model {
           'https://ovicio.com.br/wp-content/uploads/dragon-ball-super-vegeta.jpeg',
       'price': price
     };
-    http.post('https://flutter-products-bd653.firebaseio.com/products.json',
-        body: json.encode(productData));
-
-    final Product newProduct = Product(
-        title: title,
-        description: description,
-        price: price,
-        image: image,
-        userEmail: _authenticatedUser.email,
-        userId: _authenticatedUser.id);
-    _products.add(newProduct);
-    notifyListeners();
+    http
+        .post('https://flutter-products-bd653.firebaseio.com/products.json',
+            body: json.encode(productData))
+        .then((http.Response response) {
+      final Map<String, dynamic> responseData = json.decode(response.body);
+      final Product newProduct = Product(
+          id: responseData['name'],
+          title: title,
+          description: description,
+          price: price,
+          image: image,
+          userEmail: _authenticatedUser.email,
+          userId: _authenticatedUser.id);
+      _products.add(newProduct);
+      notifyListeners();
+    });
   }
 }
 
